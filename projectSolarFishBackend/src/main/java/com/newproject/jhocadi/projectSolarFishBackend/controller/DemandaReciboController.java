f470ce21527c46a611b3805ff73088a14d681018
Jhossuant0520 <jcabezas45@unisalle.edu.co>
2026-03-09 23:04:24 -0500

    corregirErrores

--- projectSolarFishBackend/src/main/java/com/newproject/jhocadi/projectSolarFishBackend/controller/DemandaReciboController.java
+++ projectSolarFishBackend/src/main/java/com/newproject/jhocadi/projectSolarFishBackend/controller/DemandaReciboController.java
@@ -2,8 +2,6 @@ package com.newproject.jhocadi.projectSolarFishBackend.controller;
 
 public class DemandaReciboController {
     
-   HOLA JAFET
-   COMO ESTAS
-   Karen yo estoy bien, espero que tu tambien estes bien, saludos
+ 
 }
 
