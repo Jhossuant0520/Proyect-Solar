a9467efc8b4fac7fedfe202544a1ae92917657a2
Karen <karendayannasanchez.18@gmail.com>
2026-03-10 22:14:05 -0500

    cambios

--- projectSolarFishBackend/src/main/java/com/newproject/jhocadi/projectSolarFishBackend/controller/DemandaReciboController.java
+++ projectSolarFishBackend/src/main/java/com/newproject/jhocadi/projectSolarFishBackend/controller/DemandaReciboController.java
@@ -2,10 +2,5 @@ package com.newproject.jhocadi.projectSolarFishBackend.controller;
 
 public class DemandaReciboController {
     
-   HOLA JAFET
-   COMO ESTAS
-   Karen yo estoy bien, espero que tu tambien estes bien, saludos
-   BIENNN
-SSSSS
 }
 
