8bd52dda0b8d281122e337fb02fbc70ab677ed7e
Karen <karendayannasanchez.18@gmail.com>
2026-03-11 23:36:05 -0500

    TS

--- projectSolarFishFrontend/src/app/homepage/home-page/components/hero-homepage/hero-homepage.ts
+++ projectSolarFishFrontend/src/app/homepage/home-page/components/hero-homepage/hero-homepage.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-hero-homepage',
+  standalone: true,
   imports: [CommonModule],
   templateUrl: './hero-homepage.html',
-  styleUrl: './hero-homepage.scss'
+  styleUrls: ['./hero-homepage.scss']
 })
 export class HeroHomepage {
 
