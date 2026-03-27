a0abf0f0963dc644d6a99ce7b0e87fe89e312fd7
Jhossuant0520 <jcabezas45@unisalle.edu.co>
2026-03-27 17:40:18 -0500

    actu2.2

--- projectSolarFishFrontend/src/app/homepage/home-page/home-page.ts
+++ projectSolarFishFrontend/src/app/homepage/home-page/home-page.ts
@@ -11,20 +11,12 @@ import { StatsHomepage } from "./components/stats-homepage/stats-homepage";
 import { StepsHomepage } from "./components/steps-homepage/steps-homepage";
 import { TestimonialsHomepage } from "./components/testimonials-homepage/testimonials-homepage";
 import { FaqHomepage } from "./components/faq-homepage/faq-homepage";
-<<<<<<< HEAD
-import { Contact } from '../contact/contact';
-
-@Component({
-  selector: 'app-home-page',
-  imports: [MatCardModule, HeroHomepage, AboutHomepage, NavbarHomepage, ServicesHomepage, Catalog, FooterHomepage,   Contact, CommonModule, StatsHomepage, StepsHomepage, TestimonialsHomepage, FaqHomepage],
-=======
 import { CtaHomepage } from "./components/cta-homepage/cta-homepage";
 import { Contact } from './components/contact/contact';
 
 @Component({
   selector: 'app-home-page',
   imports: [MatCardModule, HeroHomepage, AboutHomepage, NavbarHomepage, ServicesHomepage, Catalog, FooterHomepage, CommonModule, StatsHomepage, StepsHomepage, TestimonialsHomepage, FaqHomepage, CtaHomepage, Contact],
->>>>>>> 57bf78d9f69ab819e6e00afc78789662bfa23ee0
   templateUrl: './home-page.html',
   styleUrls: ['./home-page.scss']
 })
