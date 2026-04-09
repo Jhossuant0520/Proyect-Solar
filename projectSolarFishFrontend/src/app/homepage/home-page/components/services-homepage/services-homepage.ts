7ee3af4fb34de7ab521945c7fcad8e27aa824b78
Karen <karendayannasanchez.18@gmail.com>
2026-04-09 00:27:33 -0500

    actu2.9

--- projectSolarFishFrontend/src/app/homepage/home-page/components/services-homepage/services-homepage.ts
+++ projectSolarFishFrontend/src/app/homepage/home-page/components/services-homepage/services-homepage.ts
@@ -6,7 +6,7 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './services-homepage.html',
-  styleUrl: './services-homepage.scss'
+  styleUrls: ['./services-homepage.scss']
 })
 export class ServicesHomepage {
 
